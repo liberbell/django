2326cd6dc34d8cabf315a7ad8154a8181fdaae88
liberbell <liberbell@gmail.com>
2018-08-22 16:21:29 +0900

    model1

---
+++ adoptions/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Pet(models.Model):
+    SEX_CHOICES = [('M', 'Male'),('F', 'Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30)
+    description = models.TextField()
+    sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
+    submission_date = models.DatetimeField()
+    age = models.IntegerField(null=True)
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=50)
