99ac4b4f001698282573312e6d826436717a6714
liberbell <liberbell@gmail.com>
2018-08-24 07:46:55 +0900

    rul1

--- wisdompets/urls.py
+++ wisdompets/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls import url
+from adoptions import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.home, name='name'),
+    url(r)
 ]
