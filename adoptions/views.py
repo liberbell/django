880ca3bbd88d8fb12f2d9444508c864464021cc1
liberbell <liberbell@gmail.com>
2018-08-24 19:24:07 +0900

    view3

--- adoptions/views.py
+++ adoptions/views.py
@@ -5,5 +5,8 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('<p>home view</p>')
 
+# def pet_detail(request, id):
+#     return HttpResponse('<p>pet_detail view with the id {}</p>')
+
 def pet_detail(request, id):
-    return HttpResponse('<p>pet_detail view with the id {}</p>')
+    return HttpResponse('<p>pet_detail view with the id {}</p>'.format(id))
