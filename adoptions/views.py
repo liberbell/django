7a21d9428928d8dc3b315e9c1f7e0ae47e722545
liberbell <liberbell@gmail.com>
2018-08-25 19:39:18 +0900

    view4

--- adoptions/views.py
+++ adoptions/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+
+from .models import Pet
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<p>home view</p>')
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {'pets': pets})
+    # return HttpResponse('<p>home view</p>')
 
 # def pet_detail(request, id):
 #     return HttpResponse('<p>pet_detail view with the id {}</p>')
 
 def pet_detail(request, id):
-    return HttpResponse('<p>pet_detail view with the id {}</p>'.format(id))
+    try:
+        pets = Pet.objects.get(id=id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+    return render(request, 'pet_detail.html', {'pet': pet})
+    # return HttpResponse('<p>pet_detail view with the id {}</p>'.format(id))
